8373d2ac887c52c1df08480a8a9a7e57609b5267
florencialarreategui <florencialarreategui@gmail.com>
2022-09-26 12:25:13 -0300

    agrego componentes

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Header from "./components/header";
+import StartGameScreen from "./screens/start-game";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F4D06F',
+  
+  },
+});
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Adivina el numero"></Header>
+      <StartGameScreen/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, StyleSheet} from "react-native";
+import { colors } from "../constants/colors"
+const styles = StyleSheet.create({
+    header:{
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: colors.amarillo,
+        padingTop: 20,
+    },
+    title: {
+        fontSize: 20,
+        color: "#FF8811",
+        fontWeight: "bold"
+    }
+})
+const Header = ({ title }) => {
+    return (
+        <View style = {styles.header}>
+            <Text style={styles.title}>{title}</Text>
+        </View>
+    )
+}
+export default Header;
