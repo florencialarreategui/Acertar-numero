64b7575959e656d02963d59e3e8549402a095e4b
florencialarreategui <florencialarreategui@gmail.com>
2022-09-26 22:06:36 -0300

    importo use State

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, {useState} from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import Header from "./components/header";
 import StartGameScreen from "./screens/startGame";
