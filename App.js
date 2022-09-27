517d55b0acc683ee4517eb57647c712095397c17
florencialarreategui <florencialarreategui@gmail.com>
2022-09-26 22:01:15 -0300

    paso callbacks a app

--- App.js
+++ App.js
@@ -10,10 +10,19 @@ const styles = StyleSheet.create({
   },
 });
 export default function App() {
+  const [userNumber, setUserNumber] = useState(0);
+  const title = !userNumber ? "adivina un numero" : "Comienza el juego"
+  const  onStartGame = (selectedNumber) =>{
+    setUserNumber(selectedNumber)
+  }
+  let content = <StartGameScreen onStartGame={onStartGame} />
+  if(userNumber) {
+    content = <GameScreen selectedNumber= {userNumber}/>
+  }
   return (
     <View style={styles.container}>
       <Header title="Adivina el numero"></Header>
-      <StartGameScreen/>
+      {content}
     </View>
   );
 }
